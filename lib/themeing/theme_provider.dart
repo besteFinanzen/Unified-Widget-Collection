d34e44fe18db3da352d4bb6e6ed9a85330cd47f5
beste_Finanzen <niklas@kimmerles.de>
2024-03-08 23:12:33 +0100

    Changed to ThemeData

--- lib/themeing/theme_provider.dart
+++ lib/themeing/theme_provider.dart
@@ -1,47 +1,50 @@
 import 'package:flutter/material.dart';
 
-class _ThemeSwitcher extends InheritedWidget {
-  final DynamicThemeWidgetState data;
+class _ThemeDataSwitcher extends InheritedWidget {
+  final DynamicThemeDataWidgetState data;
 
-  const _ThemeSwitcher({
+  const _ThemeDataSwitcher({
     required this.data,
     required super.child,
   });
 
-  static DynamicThemeWidgetState of(context) {
-    return (context.dependOnInheritedWidgetOfExactType<_ThemeSwitcher>()
-    as _ThemeSwitcher)
+  static DynamicThemeDataWidgetState of(context) {
+    return (context.dependOnInheritedWidgetOfExactType<_ThemeDataSwitcher>()
+    as _ThemeDataSwitcher)
         .data;
   }
 
   @override
-  bool updateShouldNotify(_ThemeSwitcher oldWidget) {
+  bool updateShouldNotify(_ThemeDataSwitcher oldWidget) {
     return this != oldWidget;
   }
 }
 
 /// This widget is used to switch the theme of the app at runtime.
-class DynamicThemeWidget extends StatefulWidget {
-  final ColorScheme? lightColorScheme;
-  final ColorScheme darkColorScheme;
+/// Make sure you set the [DynamicThemeDataWidget] as parent of a [MaterialApp] or [CupertinoApp].
+/// These should have the [themeMode] set to ThemeSwitcher.of(context).themeMode.
+/// ALso the theme should be set to ThemeSwitcher.of(context).getThemeData(ThemeSwitcher.of(context).colorScheme).
+class DynamicThemeDataWidget extends StatefulWidget {
+  final ThemeData? lightThemeData;
+  final ThemeData darkThemeData;
   final ThemeMode themeMode;
   final Widget child;
 
-  const DynamicThemeWidget({required this.darkColorScheme, this.lightColorScheme, required this.child, this.themeMode = ThemeMode.system, super.key});
+  const DynamicThemeDataWidget({required this.darkThemeData, this.lightThemeData, required this.child, this.themeMode = ThemeMode.system, super.key});
 
   @override
-  DynamicThemeWidgetState createState() => DynamicThemeWidgetState();
+  DynamicThemeDataWidgetState createState() => DynamicThemeDataWidgetState();
 }
 
-class DynamicThemeWidgetState extends State<DynamicThemeWidget> {
-  ColorScheme? darkColorScheme;
-  ColorScheme? lightColorScheme;
+class DynamicThemeDataWidgetState extends State<DynamicThemeDataWidget> {
+  ThemeData? darkColorScheme;
+  ThemeData? lightColorScheme;
   ThemeMode? themeMode;
 
   /// This method sets the theme to the given theme.
-  /// Which theme is set is determined by the brightness of the [theme].
+  /// Which theme is set is determined by the brightness attribute of the Theme.
   /// Keep in mind that the themeMode is not changed by this method.
-  void setTheme(final ColorScheme theme) {
+  void setThemeData(final ThemeData theme) {
     if (theme.brightness == Brightness.light) {
       setState(() {
         lightColorScheme = theme;
@@ -53,6 +56,23 @@ class DynamicThemeWidgetState extends State<DynamicThemeWidget> {
     }
   }
 
+  ThemeData getThemeData(ColorScheme? colorScheme) {
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: colorScheme,
+      appBarTheme: AppBarTheme(
+        iconTheme: Theme.of(context).iconTheme.copyWith(
+          size: 30,
+        ),
+      ),
+      cardTheme: CardTheme(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+  }
+
   /// This method sets the themeMode to the given themeMode.
   void switchThemeMode(ThemeMode themeMode) async {
     setState(() {
@@ -62,10 +82,10 @@ class DynamicThemeWidgetState extends State<DynamicThemeWidget> {
 
   @override
   Widget build(BuildContext context) {
-    darkColorScheme = darkColorScheme ?? widget.darkColorScheme;
-    lightColorScheme = lightColorScheme ?? widget.lightColorScheme;
+    darkColorScheme = darkColorScheme ?? widget.darkThemeData;
+    lightColorScheme = lightColorScheme ?? widget.lightThemeData;
     themeMode = themeMode ?? widget.themeMode;
-    return _ThemeSwitcher(
+    return _ThemeDataSwitcher(
       data: this,
       child: widget.child,
     );
