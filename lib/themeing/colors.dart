fc278c30212e481a0c3ecfdfaddb0db424d52113
beste_Finanzen <niklas@kimmerles.de>
2024-03-09 23:44:09 +0100

    Added contrast color

--- lib/themeing/colors.dart
+++ lib/themeing/colors.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-extension ContrastColor on Brightness {
+extension ColorForBrigntess on Brightness {
   Color get color {
     return this == Brightness.light ? Colors.white : Colors.black;
+  }
+}
+
+extension ContrastForColor on Color {
+  Color get contrastColor {
+    return computeLuminance() > 0.5 ? Colors.black : Colors.white;
   }
 }
