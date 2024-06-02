25f0349a41d6561c3c616485429cc5690c3fe9dc
beste_Finanzen <niklas@kimmerles.de>
2024-06-02 22:38:55 +0200

    tests

--- lib/src/app_layout/pages/provider.dart
+++ lib/src/app_layout/pages/provider.dart
@@ -35,6 +35,7 @@ class TabViewProvider extends UnifiedProvider {
       return;
     }
     lastIndexOfPage = currentPageIndex;
+    print('Page changed to $currentPageIndex');
     if (_pageConfigurations[currentPageIndex].navigatorKey.currentState != null) {
       AppBarProvider.of(context).setCurrentNavigator(_pageConfigurations[currentPageIndex].navigatorKey.currentState!);
     }
